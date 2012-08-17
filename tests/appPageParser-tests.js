3e47e36f2d77741e13944ceba531631500e9c134
Johan Öbrink <johan.obrink@24hr.se>
2012-08-17 13:55:01 +0200

    Html and test changes Added some html Changed test so that it no longer calls facebook

--- tests/appPageParser-tests.js
+++ tests/appPageParser-tests.js
@@ -1,10 +1,31 @@
 var expect = require('chai').expect;
 
 describe('appPageParser', function() {
+
+	var parser;
+
+	beforeEach(function(done) {
+
+		parser = require('../lib/appPageParser');
+
+		require('fs').readFile(__dirname + '/app-page.html', function(err, data) {
+			
+			if(err) {
+				throw err;
+			}
+
+			parser.getAppHtml = function(appid, callback) {
+				callback(null, data);
+			};
+
+			done();
+		
+		});
+	
+	});
 	
 	it('should scrape the url of the GO TO APP button', function(done) {
 		
-		var parser = require('../lib/appPageParser');
 		parser.getAppUrl(441992625824469, function(err, result) {
 
 			expect(result).to.equal('http://www.facebook.com/minfrukost/app_441992625824469');
